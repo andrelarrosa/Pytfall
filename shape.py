58bfff4aac052cdb1d11f4739c62ba8129f4011b
andrelarrosa <andre.larrosa@outlook.com>
2023-06-20 22:23:22 -0300

    Criando o shape do retangulo para fazer a hitbox

--- shape.py
+++ shape.py
@@ -2,5 +2,3 @@ class Shape:
     def __init__(self):
         self.dx = 0
         self.dy = 0
-        self.x = 0
-        self.y = 0
---
+++ shapeRectangle.py
@@ -0,0 +1,6 @@
+from shape import Shape
+
+class ShapeRectangle (Shape):
+    def __init__(self):
+        self.b = 0.0
+        self.h = 0.0
